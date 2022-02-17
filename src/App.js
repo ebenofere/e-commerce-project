f8a7b2f3bc0159e386918f9bf2a61781be10fbd2
Ebenezer Ofere <eoofere@sidmach.com>
2022-02-17 11:36:03 +0100

    Routing in our Project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import "./App.css";
-import "./pages/homepage/homepage.styles.scss";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />;
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/hats" element={<HatsPage />} />
+      </Routes>
     </div>
   );
 }
